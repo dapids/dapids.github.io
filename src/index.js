8e8dee85619acf5caaaba64bf93f53129d7f79bc
David Sorrentino <me@davidsorrentino.com>
2018-04-02 17:08:10 +0200

    Updates

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import registerServiceWorker from 'registerServiceWorker'
-import Home from 'UI/pages/Home'
-
-ReactDOM.render(<Home />, document.getElementById('root'))
-registerServiceWorker()
